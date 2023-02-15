85ee1af22d3b5e6b7963223899d04ed755cbe07a
ANIKET KADALE <aniketkadale18@gmail.com>
2023-02-15 23:48:04 +0530

    Adding files of webview

---
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.webview
+
+import android.os.Build
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+import com.example.webview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webViewVariable = findViewById<WebView>(R.id.webView)
+        webViewSetUp(webViewVariable)
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun webViewSetUp(webView: WebView) {
+        webView.webViewClient = WebViewClient()
+        webView.apply {
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+            loadUrl("https://aniket-kadale-portfolio.netlify.app/")
+        }
+    }
+}
